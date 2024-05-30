03b1f0780908897ce35ab957d006d5dcae3bf124
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-05-31 03:52:45 +0400

    have created ingredientsTypes and imgs for them

---
+++ src/ingredientsAll.ts
@@ -0,0 +1,12 @@
+import meatImg from "./assets/ingredientsPics/Meat.png";
+import baconImg from "./assets/ingredientsPics/Bacon.png";
+import cheeseImg from "./assets/ingredientsPics/Cheese.png";
+import lettuceImg from "./assets/ingredientsPics/Lettuce.png";
+import {ItemIngredients} from "./types";
+
+
+
+export const meatIngredient:ItemIngredients = {name:"Meat", price: 80, img: meatImg}
+export const baconIngredient:ItemIngredients = {name:"Bacon", price: 60, img: baconImg}
+export const cheeseIngredient:ItemIngredients = {name:"Cheese", price: 50, img: cheeseImg}
+export const lettuceIngredient:ItemIngredients = {name:"Lettuce", price: 10, img: lettuceImg}
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface ItemIngredients{
+    name:string,
+    price:number,
+    img:string
+}
+
+export interface IngredientsAll{
+    name: ItemIngredients,
+    count:number
+}
